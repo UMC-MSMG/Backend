140b6e34d8220f192fe08cdcd1bd65f66955bf69
sjhan0814 <sjhan0814@gmail.com>
2025-01-22 18:59:38 +0900

    feat. 카카오 로그인 구현 완료

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -5,7 +5,9 @@ import {
   login,
   kakaoLogin,
 } from "../controllers/auth.controller";
+import passport from "passport";
 
+///api/auth
 const router = Router();
 
 // 전화번호 인증 요청
@@ -14,10 +16,27 @@ router.post("/phone/verify", requestPhoneVerification);
 // 인증 코드 확인
 router.post("/phone/verify-code", verifyPhoneCode);
 
-// 이메일/비밀번호 로그인
-router.post("/login", login);
+// 이메일/비밀번호 일반 로그인
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
 // 카카오 로그인
-router.get("/oauth2/login/kakao", kakaoLogin);
-
+router.get("/login/kakao", passport.authenticate("kakao"));
+//콜백
+router.get(
+  "/kakao/callback",
+  passport.authenticate("kakao", {
+    failureRedirect: "/login/kakao",
+    failureMessage: true,
+  }),
+  (req, res) => {
+    res.redirect("/");
+  }
+);
 export default router;
