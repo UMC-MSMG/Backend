e6e659c1f039825cc36ccafae7f44ab38f107599
sjhan0814 <sjhan0814@gmail.com>
2025-02-08 21:51:21 +0900

    temp 삭제

--- src/repositories/tempCodeRunnerFile.ts
+++
@@ -1,9 +0,0 @@
-    const monthlyCount = await prisma.workoutRecord.groupBy({
-      by: ["completeDate"],
-      where: { userId: userId, isComplete: true },
-      having: {
-        completeDate: { lte: lastDayOfMonth, gte: firstDayOfMonth },
-      },
-      _count: { id: true },
-    });
-    console.log(monthlyCount);
